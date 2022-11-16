7382acaa11c7bf6d8021583d19b2f17653dbcd6b
Aviya Yechezkel <aviyay@edu.acc.ac.il>
2022-11-16 11:31:51 +0200

    fixed convention

--- src/InvoiceSeries.java
+++ src/InvoiceSeries.java
@@ -1,24 +1,24 @@
 import java.util.Scanner;
 
 public class InvoiceSeries {
-    public static void main(String[] args){
-                float firstNumber;
-                float distance;
-                int numberOfObjects;
-                Scanner scanner= new Scanner(System.in);
-                System.out.println("Enter a number you want to start with ");
-                firstNumber=scanner.nextFloat();
-                System.out.println("Enter the distance between the following objects ");
-                distance=scanner.nextFloat();
-                System.out.println("Enter the amount of objects in the series ");
-                numberOfObjects=scanner.nextInt();
-                System.out.print("the series is: "+firstNumber+"\t");
-                for (int i=0;i<numberOfObjects;i++){
-                    System.out.print((firstNumber+=distance)+"\t");
-                }
-
-
-            }
+    public static void main(String[] args) {
+        float firstNumber;
+        float distance;
+        float numberOfObjects;
+        Scanner scanner= new Scanner(System.in);
+        System.out.println("Enter a number you want to start with ");
+        firstNumber=scanner.nextFloat();
+        System.out.println("Enter the distance between the following objects ");
+        distance=scanner.nextFloat();
+        do{
+            System.out.println("Enter the amount of objects in the series ");
+            numberOfObjects=scanner.nextFloat();
+        }while(numberOfObjects<0||numberOfObjects%1!=0);
+        System.out.print("the series is: "+"("+firstNumber+")"+"\t");
+        for (int i=1;i<numberOfObjects;i++){
+            System.out.print("("+(firstNumber+=distance)+")"+"\t");
         }
 
+
+    }}
 
--- src/QuadraticEquation.java
+++ src/QuadraticEquation.java
@@ -1,32 +1,34 @@
 import java.util.Scanner;
 
 public class QuadraticEquation {
-    public  static void main(String[] args) {
-        double x1, x2,sum = 0;
-        double valuesOf[] = new double[3];
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        for (int i = 0; i < 3; i++) {
-            System.out.println("please enter a number");
-            valuesOf[i] = scanner.nextInt();
-        }
-        if (valuesOf[0] == 0) {
-            x1 = valuesOf[2] / valuesOf[1];
-            System.out.println("There is only one solution to the eqution:" + x1*-1);
-        } else if (valuesOf[1]==0&&valuesOf[2]==0) {
-            System.out.println("There are only one solution to the eqution: 0");
-
-        }
-        else if ((valuesOf[1] * valuesOf[1]) - 4 * valuesOf[0] * valuesOf[2]==0){
-            System.out.println("There are only one solution to the eqution:" + valuesOf[1]/(2*valuesOf[0]));
-        }
-         else if ((valuesOf[1] * valuesOf[1]) - 4 * valuesOf[0] * valuesOf[2] < 0) {
-            System.out.println("There is no solution to the eqution");
+        double firstNumberA;
+        double secondNumberB;
+        double thirdNumberC;
+        double delta;
+        double x1;
+        double x2;
+        System.out.println("Please enter the first number");
+        firstNumberA = scanner.nextDouble();
+        System.out.println("Please enter the second number");
+        secondNumberB = scanner.nextDouble();
+        System.out.println("Please enter the third number");
+        thirdNumberC = scanner.nextDouble();
+        delta = ((secondNumberB * secondNumberB) - 4 * firstNumberA * thirdNumberC);
+        if (firstNumberA == 0 && secondNumberB != 0 && thirdNumberC != 0) {
+            System.out.println("There is only one solution to the equation: " + (thirdNumberC / secondNumberB) * -1);
+        } else if (delta < 0 || (firstNumberA == 0 && secondNumberB == 0 && thirdNumberC != 0)) {
+            System.out.println("There is no solution to the equation");
+        } else if (firstNumberA == 0 && thirdNumberC == 0 && secondNumberB != 0) {
+            System.out.println("There is only one solution to the equation: 0");
+        } else if (delta==0) {
+            System.out.println("There is only one solution to the equation: " + secondNumberB/(2*firstNumberA));
         } else {
-            sum = ((valuesOf[1]) - 4 * valuesOf[0] * valuesOf[2]);
-                x1 = (valuesOf[1] + Math.sqrt(sum))/(2 * valuesOf[0]);
-                x2 = (valuesOf[1] - Math.sqrt(sum))/(2 * valuesOf[0]);
-                System.out.println("There are two solutions to the eqution:" + "x1=" + x1 +" "+ "x2=" + x2);
-
+            x1 = (secondNumberB - (Math.sqrt(delta)) / (2 * firstNumberA));
+            x2 = (secondNumberB + (Math.sqrt(delta)) / (2 * firstNumberA));
+            System.out.println("There are two solutions to the equation:" + "x1= " + x1 + " x2= " + x2);
         }
 
-    }}
+    }
+}
