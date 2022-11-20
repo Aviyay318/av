4d2b54de8a8d9e835b231e4e11a5ac3b55d1dc2c
Aviya Yechezkel <aviyay@edu.acc.ac.il>
2022-11-20 13:41:50 +0200

    completed

--- src/Exercise1Part2.java
+++ src/Exercise1Part2.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Exercise1 {
+public class Exercise1Part2 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         double firstNumberA;
